2721abd78e25edaa6c44b7cd48ca136a3753b997
Andrew Robertson <andy.d.robertson@gmail.com>
2020-05-25 18:02:35 +0100

    deal with mastodon null responses

--- mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/method/Streaming.kt
+++ mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/method/Streaming.kt
@@ -67,7 +67,15 @@ class Streaming(private val client: MastodonClient) {
                 while (true) {
                     try{
                         val line = reader.readLine()
-                        if (line == null || line.isEmpty()) {
+                        if (line == null){
+                            // Stream seems not to recover from receiving nulls from mastodon, so restarting
+                            handler.log("Mastodon response produced null, restarting connection...")
+                            // Sometimes mastodon instance just messes up stream, try to re-establish connection...
+                            response = client.get("streaming/public/local")
+                            reader = response.body().byteStream().bufferedReader()
+                            continue
+                        }
+                        if (line.isEmpty()) {
                             continue
                         }
                         val type = line.split(":")[0].trim()
@@ -166,7 +174,18 @@ class Streaming(private val client: MastodonClient) {
                 while (true) {
                     try{
                         val line = reader.readLine()
-                        if (line == null || line.isEmpty()) {
+                        if (line == null){
+                            // Stream seems not to recover from receiving nulls from mastodon, so restarting
+                            handler.log("Mastodon response produced null, restarting connection...")
+                            // Sometimes mastodon instance just messes up stream, try to re-establish connection...
+                            response = client.get(
+                                    "streaming/hashtag",
+                                    Parameter().append("tag", tag)
+                            )
+                            reader = response.body().byteStream().bufferedReader()
+                            continue
+                        }
+                        if (line.isEmpty()) {
                             continue
                         }
                         val type = line.split(":")[0].trim()
